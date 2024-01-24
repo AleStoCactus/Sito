9dfc5743bf6875b8602027ffc7c0508deddf61b8
AleStoCactus <alessandropala06@gmail.com>
2024-01-24 10:56:36 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function auth() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if (username == "12345" && password == "admin") {
+		alert("Login Successfully");
+		window.location.assign("storia.html");
+	}
+	else {
+		alert("Invalid Information");
+		document.title = "Caught your pay-card xd";
+		return;
+	}
+}
+function lag() {
+	alert("Non e'.. poss.. ch... sono.. riusc. to.. a.. fare... quest.. sito..")
+}
+function forgot() {
+	alert("Username: 12345, Password: admin")
+}
+function home() {
+	window.location.assign("index.html");
+}
